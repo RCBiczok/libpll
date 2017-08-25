49752fdaa55d55eaeabe2b6afc3107ea1903c1dc
Rudolf Biczok <rcbiczok@gmail.com>
2017-08-25 11:02:32 +0200

    Use AVX2 to process the last rows

--- src/core_partials_avx512f.c
+++ src/core_partials_avx512f.c
@@ -23,6 +23,38 @@
 
 #define ELEM_PER_AVX515_REGISTER 8
 
+#define PROCESS_ROWS_AVX2(j)                           \
+  v_lclv = _mm256_load_pd(left_clv + j);               \
+  v_rclv = _mm256_load_pd(right_clv + j);              \
+  /* row 0 */                                          \
+  v_mat = _mm256_load_pd(lm0);                         \
+  v_terma0 = _mm256_fmadd_pd(v_mat, v_lclv, v_terma0); \
+  v_mat = _mm256_load_pd(rm0);                         \
+  v_termb0 = _mm256_fmadd_pd(v_mat, v_rclv, v_termb0); \
+  lm0 += 4;                                            \
+  rm0 += 4;                                            \
+  /* row 1 */                                          \
+  v_mat = _mm256_load_pd(lm1);                         \
+  v_terma1 = _mm256_fmadd_pd(v_mat, v_lclv, v_terma1); \
+  v_mat = _mm256_load_pd(rm1);                         \
+  v_termb1 = _mm256_fmadd_pd(v_mat, v_rclv, v_termb1); \
+  lm1 += 4;                                            \
+  rm1 += 4;                                            \
+  /* row 2 */                                          \
+  v_mat = _mm256_load_pd(lm2);                         \
+  v_terma2 = _mm256_fmadd_pd(v_mat, v_lclv, v_terma2); \
+  v_mat = _mm256_load_pd(rm2);                         \
+  v_termb2 = _mm256_fmadd_pd(v_mat, v_rclv, v_termb2); \
+  lm2 += 4;                                            \
+  rm2 += 4;                                            \
+  /* row 3 */                                          \
+  v_mat = _mm256_load_pd(lm3);                         \
+  v_terma3 = _mm256_fmadd_pd(v_mat, v_lclv, v_terma3); \
+  v_mat = _mm256_load_pd(rm3);                         \
+  v_termb3 = _mm256_fmadd_pd(v_mat, v_rclv, v_termb3); \
+  lm3 += 4;                                            \
+  rm3 += 4;                                            \
+
 
 #define PROCESS_8_ROWS_HALF(j)                         \
   v_lclv = _mm512_load_pd(left_clv + j);               \
@@ -544,16 +576,9 @@ void pll_core_update_partial_ii_20x20_avx512f(unsigned int sites,
   unsigned int scale_mask;
   unsigned int init_mask;
   __m512d v_scale_threshold = _mm512_set1_pd(PLL_SCALE_THRESHOLD);
+  __m256d v_scale_threshold_256 = _mm256_set1_pd(PLL_SCALE_THRESHOLD);
   __m512d v_scale_factor = _mm512_set1_pd(PLL_SCALE_FACTOR);
 
-  __m512i switch_256lanes_mask = _mm512_setr_epi64(4,
-                                               5,
-                                               6,
-                                               7,
-                                               1,
-                                               2,
-                                               3,
-                                               4);
   __m512i permute_mask = _mm512_setr_epi64(0 | 4,
                                            0 | 5,
                                            0 | 6,
@@ -597,18 +622,18 @@ void pll_core_update_partial_ii_20x20_avx512f(unsigned int sites,
       PROCESS_8_COLUMNS(0);
       PROCESS_8_COLUMNS(8);
 
-      __m512d v_terma0 = _mm512_setzero_pd();
-      __m512d v_termb0 = _mm512_setzero_pd();
-      __m512d v_terma1 = _mm512_setzero_pd();
-      __m512d v_termb1 = _mm512_setzero_pd();
-      __m512d v_terma2 = _mm512_setzero_pd();
-      __m512d v_termb2 = _mm512_setzero_pd();
-      __m512d v_terma3 = _mm512_setzero_pd();
-      __m512d v_termb3 = _mm512_setzero_pd();
+      __m256d v_terma0 = _mm256_setzero_pd();
+      __m256d v_termb0 = _mm256_setzero_pd();
+      __m256d v_terma1 = _mm256_setzero_pd();
+      __m256d v_termb1 = _mm256_setzero_pd();
+      __m256d v_terma2 = _mm256_setzero_pd();
+      __m256d v_termb2 = _mm256_setzero_pd();
+      __m256d v_terma3 = _mm256_setzero_pd();
+      __m256d v_termb3 = _mm256_setzero_pd();
 
-      __m512d v_mat;
-      __m512d v_lclv;
-      __m512d v_rclv;
+      __m256d v_mat;
+      __m256d v_lclv;
+      __m256d v_rclv;
 
       /* point to the four rows of the left matrix */
       const double *lm0 = lmat;
@@ -622,56 +647,56 @@ void pll_core_update_partial_ii_20x20_avx512f(unsigned int sites,
       const double *rm2 = rm1 + states_padded;
       const double *rm3 = rm2 + states_padded;
 
-      PROCESS_8_ROWS_HALF(0)
-      PROCESS_8_ROWS_HALF(8)
-      PROCESS_8_ROWS_HALF(16)
+      /* iterate over quadruples of columns */
+      PROCESS_ROWS_AVX2(0);
+      PROCESS_ROWS_AVX2(4);
+      PROCESS_ROWS_AVX2(8);
+      PROCESS_ROWS_AVX2(12);
+      PROCESS_ROWS_AVX2(16);
 
       /* point pmatrix to the next four rows */
-      lmat += 8 * states_padded;
-      rmat += 8 * states_padded;
+      lmat += 8*states_padded;
+      rmat += 8*states_padded;
 
-      __m512d xmm0 = _mm512_unpackhi_pd(v_terma0, v_terma1);
-      __m512d xmm1 = _mm512_unpacklo_pd(v_terma0, v_terma1);
+      __m256d xmm0 = _mm256_unpackhi_pd(v_terma0, v_terma1);
+      __m256d xmm1 = _mm256_unpacklo_pd(v_terma0, v_terma1);
 
-      __m512d xmm2 = _mm512_unpackhi_pd(v_terma2, v_terma3);
-      __m512d xmm3 = _mm512_unpacklo_pd(v_terma2, v_terma3);
+      __m256d xmm2 = _mm256_unpackhi_pd(v_terma2, v_terma3);
+      __m256d xmm3 = _mm256_unpacklo_pd(v_terma2, v_terma3);
 
-      xmm0 = _mm512_add_pd(xmm0, xmm1);
-      xmm1 = _mm512_add_pd(xmm2, xmm3);
+      xmm0 = _mm256_add_pd(xmm0, xmm1);
+      xmm1 = _mm256_add_pd(xmm2, xmm3);
 
-      xmm2 = _mm512_permutex2var_pd(xmm0, permute_mask_final_stage, xmm1);
+      xmm2 = _mm256_permute2f128_pd(xmm0, xmm1, _MM_SHUFFLE(0, 2, 0, 1));
 
-      xmm3 = _mm512_mask_blend_pd(0xCC, xmm0, xmm1);
+      xmm3 = _mm256_blend_pd(xmm0, xmm1, 12);
 
-      __m512d blend = _mm512_add_pd(xmm2, xmm3);
-
-      __m512d v_terma_sum = _mm512_add_pd(blend, _mm512_permutexvar_pd(switch_256lanes_mask, blend));
+      __m256d v_terma_sum = _mm256_add_pd(xmm2, xmm3);
 
       /* compute termb */
 
-      xmm0 = _mm512_unpackhi_pd(v_termb0, v_termb1);
-      xmm1 = _mm512_unpacklo_pd(v_termb0, v_termb1);
+      xmm0 = _mm256_unpackhi_pd(v_termb0, v_termb1);
+      xmm1 = _mm256_unpacklo_pd(v_termb0, v_termb1);
 
-      xmm2 = _mm512_unpackhi_pd(v_termb2, v_termb3);
-      xmm3 = _mm512_unpacklo_pd(v_termb2, v_termb3);
+      xmm2 = _mm256_unpackhi_pd(v_termb2, v_termb3);
+      xmm3 = _mm256_unpacklo_pd(v_termb2, v_termb3);
 
-      xmm0 = _mm512_add_pd(xmm0, xmm1);
-      xmm1 = _mm512_add_pd(xmm2, xmm3);
+      xmm0 = _mm256_add_pd(xmm0, xmm1);
+      xmm1 = _mm256_add_pd(xmm2, xmm3);
 
-      xmm2 = _mm512_permutex2var_pd(xmm0, permute_mask_final_stage, xmm1);
+      xmm2 = _mm256_permute2f128_pd(xmm0, xmm1, _MM_SHUFFLE(0, 2, 0, 1));
 
-      xmm3 = _mm512_mask_blend_pd(0xCC, xmm0, xmm1);
+      xmm3 = _mm256_blend_pd(xmm0, xmm1, 12);
 
-      blend = _mm512_add_pd(xmm2, xmm3);
+      __m256d v_termb_sum = _mm256_add_pd(xmm2, xmm3);
 
-      __m512d v_termb_sum = _mm512_add_pd(blend, _mm512_permutexvar_pd(switch_256lanes_mask, blend));
-
-      __m512d v_prod = _mm512_mul_pd(v_terma_sum, v_termb_sum);
+      __m256d v_prod = _mm256_mul_pd(v_terma_sum, v_termb_sum);
 
       /* check if scaling is needed for the current rate category */
-      rate_mask = _mm512_cmp_pd_mask(v_prod, v_scale_threshold, _CMP_LT_OS);
+      __m256d v_cmp = _mm256_cmp_pd(v_prod, v_scale_threshold_256, _CMP_LT_OS);
+      rate_mask = rate_mask & _mm256_movemask_pd(v_cmp);
 
-      _mm512_store_pd(parent_clv + 16, v_prod);
+      _mm256_store_pd(parent_clv + 16, v_prod);
 
       if (scale_mode == 2) {
         /* PER-RATE SCALING: if *all* entries of the *rate* CLV were below
